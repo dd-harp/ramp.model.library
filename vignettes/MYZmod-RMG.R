a56a487d6739030804bdd4ad1b43600326007526
David Smith <smitdave@gmail.com>
2024-03-15 15:42:46 -0700

    updates

---
+++ vignettes/MYZmod-RMG.R
@@ -0,0 +1,19 @@
+## ----echo=F, eval=F-----------------------------------------------------------
+#  devtools::load_all()
+#  devtools::load_all("../../exDE")
+
+## -----------------------------------------------------------------------------
+model <- xde_setup(MYZname="RMG", Lname="trace", Xname = "trace", nPatches=3)
+
+## -----------------------------------------------------------------------------
+model <- xde_solve(model)
+
+## ----fig.height=7.5, fig.width=5.5--------------------------------------------
+par(mfrow = c(2,1))
+xde_plot_M(model)
+xde_plot_YZ(model, add_axes = F)
+xde_plot_YZ_fracs(model)
+
+## -----------------------------------------------------------------------------
+xde_plot_YZ_fracs(model)
+
